2e5823aa9ef97de2a61c0dda462ffe2dcc3dc1d5
969684 <nathan.bily@delta.com>
2023-12-07 01:58:32 -0500

    day 7

--- advent_2023/7.py
+++ advent_2023/7.py
@@ -1,69 +1,69 @@
 # %% 1
 
-# raw = []
+raw = []
 
-# with open("advent_2023/7.txt", "r") as file:
-#     raw = [x.strip().split() for x in file if x.strip() != ""]
+with open("advent_2023/7.txt", "r") as file:
+    raw = [x.strip().split() for x in file if x.strip() != ""]
 
-# print(raw)
-
-
-# def eval_hand(inp: str) -> int:
-#     rtn_val = 0
-
-#     card_rnk = "23456789TJQKA"
-
-#     adder = 0
-
-#     for i in range(len(inp)):
-#         rtn_val += 0.01 / (1000**i) * (card_rnk.index(inp[i]) + 1)
-
-#     inp = sorted(inp)
-
-#     matches = ""
-#     sprite = 0
-#     while sprite < 5:
-#         checker = inp[sprite]
-#         repeats = 1
-#         for i in range(sprite + 1, 5):
-#             if inp[i] == checker:
-#                 repeats += 1
-#                 sprite += 1
-#         matches += f"{repeats}"
-#         sprite += 1
-
-#     if matches == "5":
-#         rtn_val += 7
-#     elif "4" in matches:
-#         rtn_val += 6
-#     elif matches in ["32", "23"]:
-#         rtn_val += 5
-#     elif "3" in matches:
-#         rtn_val += 4
-#     elif "22" in "".join(sorted(matches)):
-#         rtn_val += 3
-#     elif "2" in matches:
-#         rtn_val += 2
-#     else:
-#         rtn_val += 1
-
-#     return rtn_val
+print(raw)
 
 
-# for i in range(len(raw)):
-#     raw[i][1] = int(raw[i][1])
-#     raw[i] += [eval_hand(raw[i][0])]
+def eval_hand(inp: str) -> int:
+    rtn_val = 0
 
-# values = sorted([x[2] for x in raw])
-# for i in range(len(raw)):
-#     raw[i] += [values.index(raw[i][2]) + 1]
+    card_rnk = "23456789TJQKA"
 
-# tot_value = 0
-# for x in raw:
-#     tot_value += x[1] * x[3]
+    adder = 0
 
-# print(raw)
-# print(tot_value)
+    for i in range(len(inp)):
+        rtn_val += 0.01 / (1000**i) * (card_rnk.index(inp[i]) + 1)
+
+    inp = sorted(inp)
+
+    matches = ""
+    sprite = 0
+    while sprite < 5:
+        checker = inp[sprite]
+        repeats = 1
+        for i in range(sprite + 1, 5):
+            if inp[i] == checker:
+                repeats += 1
+                sprite += 1
+        matches += f"{repeats}"
+        sprite += 1
+
+    if matches == "5":
+        rtn_val += 7
+    elif "4" in matches:
+        rtn_val += 6
+    elif matches in ["32", "23"]:
+        rtn_val += 5
+    elif "3" in matches:
+        rtn_val += 4
+    elif "22" in "".join(sorted(matches)):
+        rtn_val += 3
+    elif "2" in matches:
+        rtn_val += 2
+    else:
+        rtn_val += 1
+
+    return rtn_val
+
+
+for i in range(len(raw)):
+    raw[i][1] = int(raw[i][1])
+    raw[i] += [eval_hand(raw[i][0])]
+
+values = sorted([x[2] for x in raw])
+for i in range(len(raw)):
+    raw[i] += [values.index(raw[i][2]) + 1]
+
+tot_value = 0
+for x in raw:
+    tot_value += x[1] * x[3]
+
+print(raw)
+print(tot_value)
 
 
 # %% 2
