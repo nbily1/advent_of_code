f8fc6a6c2f17fa8ea122b96bfc22f491da4b5832
969684 <nathan.bily@delta.com>
2024-12-19 08:56:30 -0500

    2024 day 19

--- advent_2024/19.py
+++ advent_2024/19.py
@@ -2,35 +2,35 @@ from functools import cache
 
 # %% Part 1
 
-# with open("advent_2024/19.txt", "r") as file:
-#     patterns = [x.strip().split(", ") for x in file if "," in x][0]
+with open("advent_2024/19.txt", "r") as file:
+    patterns = [x.strip().split(", ") for x in file if "," in x][0]
 
-# with open("advent_2024/19.txt", "r") as file:
-#     designs = [x.strip() for x in file if "," not in x and x.strip() != ""]
+with open("advent_2024/19.txt", "r") as file:
+    designs = [x.strip() for x in file if "," not in x and x.strip() != ""]
 
-# print(patterns)
-# print(designs)
-
-
-# @cache
-# def checker(design: str) -> bool:
-#     for i in range(len(design), 0, -1):
-#         to_check = design[:i]
-#         if to_check in patterns:
-#             if i == len(design):
-#                 return True
-#             else:
-#                 if checker(design[i:]):
-#                     return True
-#     return False
+print(patterns)
+print(designs)
 
 
-# possible = 0
-# for d in designs:
-#     if checker(d):
-#         possible += 1
+@cache
+def checker(design: str) -> bool:
+    for i in range(len(design), 0, -1):
+        to_check = design[:i]
+        if to_check in patterns:
+            if i == len(design):
+                return True
+            else:
+                if checker(design[i:]):
+                    return True
+    return False
 
-# print(possible)
+
+possible = 0
+for d in designs:
+    if checker(d):
+        possible += 1
+
+print(possible)
 
 
 # %% Part 2
